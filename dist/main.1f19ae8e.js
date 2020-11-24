b1d1183aa8dc38ab1817f02384a1931ada6bf011
Vivi <jiahui1001@yeah.net>
2020-11-25 00:26:43 +0800

    update bulid

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -178,7 +178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56239" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56283" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -355,4 +355,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/Administrator/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=dist/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
